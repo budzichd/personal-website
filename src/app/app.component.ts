64d11b8859871a24295a48d78b60702464bdb349
Dawid Budzich <dawid.budzich@wolterskluwer.com>
2024-08-15 22:19:28 +0200

    test linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// const unusedVar = '';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'personal-website';
+	title = 'personal-website';
+	constructor() {
+		if (
+			'asdadasdasdasdasdasdasdasdasddsadasdaadasddasdaddadasdsdadsasd' > this.title &&
+			this.title.length > 5
+		) {
+			console.log('longer');
+		}
+	}
 }
