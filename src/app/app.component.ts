b0db6ecd71f17e52e4fd8eaefea52717876b52cd
Dawid Budzich <dawid.budzich@wolterskluwer.com>
2024-08-15 22:20:05 +0200

    Revert "test linting"
    This reverts commit 64d11b8859871a24295a48d78b60702464bdb349.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-// const unusedVar = '';
-
 @Component({
-	selector: 'app-root',
-	standalone: true,
-	imports: [RouterOutlet],
-	templateUrl: './app.component.html',
-	styleUrl: './app.component.scss',
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-	title = 'personal-website';
-	constructor() {
-		if (
-			'asdadasdasdasdasdasdasdasdasddsadasdaadasddasdaddadasdsdadsasd' > this.title &&
-			this.title.length > 5
-		) {
-			console.log('longer');
-		}
-	}
+  title = 'personal-website';
 }
